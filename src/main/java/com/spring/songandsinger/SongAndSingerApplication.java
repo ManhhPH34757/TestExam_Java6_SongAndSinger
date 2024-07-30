a9260bd5a945568357f4425ac42444442c927e26
ManhhPH34757 <nguyenmanh2707ngt@gmail.com>
2024-07-30 17:05:29 +0700

    create project

---
+++ src/main/java/com/spring/songandsinger/SongAndSingerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.songandsinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongAndSingerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongAndSingerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/songandsinger/SongAndSingerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.songandsinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongAndSingerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
